99597eade7622bf1b60f4162b314b83d70b70890
Abram Wingle <abramwingle15@gmail.com>
2018-11-30 18:25:03 -0700

    gonna restart just dont want to f myself

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,36 +1,37 @@
 var q1a = false;
 var q2a = false;
-var q3a = false; 
+var q3a = false;
 var q4a = false;
 var q5a = false;
-var q6a = false; 
+var q6a = false;
 var q7a = false;
 var q8a = false;
 var correctAnswers;
- 
-$(document).ready(function() {
-    function reset () {
+
+$(document).ready(function () {
+    function reset() {
         correctAnswers = 0;
-         incorrectAnswers = 0;
+        incorrectAnswers = 0;
     }
 
     reset();
 
-    $(".correctQuestion").click(function(){
+    $(".correctQuestion").click(function () {
         correctAnswers++;
         console.log("correct: " + correctAnswers);
+        $(".correctQuestion").css("background-color", "Green")
     });
 
-    $(".question").click(function() {
+    $(".question").click(function () {
         incorrectAnswers++;
         console.log("incorrect: " + incorrectAnswers);
 
     });
 
-   
 
 
 
-    
+
+
 });
 
